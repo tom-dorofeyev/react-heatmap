a9e3a02301e91b1690c1246733ddd900039cbfa2
Tom Dorofeyev <tomdorofey@gmail.com>
2025-09-04 22:00:30 +0300

    converted into paramatized test

--- src/modules/common/Heatmap/color-calculator/color-calculator.test.ts
+++ src/modules/common/Heatmap/color-calculator/color-calculator.test.ts
@@ -1,42 +1,18 @@
 import { getHeatmapItemColor } from "./color-calculator";
 
 describe("getHeatmapItemColor()", () => {
-  it('returns "color1" when 1st argument is zero', () => {
-    const result = getHeatmapItemColor(0, 100);
-    expect(result).toBe("color1");
-  });
-
-  it('returns "color2" when 1st argument is less then or equal to 25%', () => {
-    const result1 = getHeatmapItemColor(1, 100);
-    const result2 = getHeatmapItemColor(25, 100);
-
-    expect(result1).toBe("color2");
-    expect(result2).toBe("color2");
-  });
-
-  it('returns "color3" when 1st argument is less then or equal to 50%', () => {
-    const result1 = getHeatmapItemColor(26, 100);
-    const result2 = getHeatmapItemColor(50, 100);
-
-    expect(result1).toBe("color3");
-    expect(result2).toBe("color3");
-  });
-
-  it('returns "color4" when 1st argument is less then or equal to 75%', () => {
-    const result1 = getHeatmapItemColor(51, 100);
-    const result2 = getHeatmapItemColor(75, 100);
-
-    expect(result1).toBe("color4");
-    expect(result2).toBe("color4");
-  });
-
-  it('returns "color5" when 1st argument is bigger then 75%', () => {
-    const result1 = getHeatmapItemColor(76, 100);
-    const result2 = getHeatmapItemColor(100, 100);
-    const result3 = getHeatmapItemColor(1000, 100);
-
-    expect(result1).toBe("color5");
-    expect(result2).toBe("color5");
-    expect(result3).toBe("color5");
+  test.each([
+    [0, 100, "color1"], // exactly 0
+    [1, 100, "color2"], // <= 25%
+    [25, 100, "color2"],
+    [26, 100, "color3"], // <= 50%
+    [50, 100, "color3"],
+    [51, 100, "color4"], // <= 75%
+    [75, 100, "color4"],
+    [76, 100, "color5"], // > 75%
+    [100, 100, "color5"],
+    [1000, 100, "color5"], // way over max
+  ])('when value is %i and max is %i return "%s"', (value, max, expected) => {
+    expect(getHeatmapItemColor(value, max)).toBe(expected);
   });
 });
